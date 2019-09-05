a3d6a0912e4a8a3bcacb8e64b69543489c6ff400
changjie <regretn@163.com>
2019-09-05 17:32:47 +0800

    fix: login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,89 +1,111 @@
-const app = getApp();
+// pages/login/login.js
 Page({
+
+  /**
+   * Page initial data
+   */
   data: {
-    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
   },
-  onLoad: function () {
-    var that = this;
-    // 查看是否授权
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  getUserInfo: function (e) {
+    let that = this;
+    // console.log(e)
+    // 获取用户信息
     wx.getSetting({
-      success: function (res) {
+      success(res) {
+        // console.log("res", res)
         if (res.authSetting['scope.userInfo']) {
-          console.log(res,'log onload res')
+          console.log("已授权=====")
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
           wx.getUserInfo({
-            success: function (res) {
-              //从数据库获取用户信息
-              that.queryUsreInfo();
-              //用户已经授权过
-              wx.switchTab({
-                url: '/pages/index/index'
+            success(res) {
+              console.log("获取用户信息成功", res)
+              that.setData({
+                name: res.userInfo.nickName
               })
+            },
+            fail(res) {
+              console.log("获取用户信息失败", res)
             }
-          });
+          })
+        } else {
+          console.log("未授权=====")
+          that.showSettingToast("请授权")
         }
       }
     })
   },
-  bindGetUserInfo: function (e) {
-    if (e.detail.userInfo) {
-      //用户按了允许授权按钮
-      var that = this;
-      //插入登录的用户的相关信息到数据库
-      console.log(app,e.detail.userInfo,'log')
-      // wx.request({
-      //   url: app.globalData.urlPath + 'user/add',
-      //   data: {
-      //     openid: getApp().globalData.openid,
-      //     nickName: e.detail.userInfo.nickName,
-      //     avatarUrl: e.detail.userInfo.avatarUrl,
-      //     province: e.detail.userInfo.province,
-      //     city: e.detail.userInfo.city
-      //   },
-      //   header: {
-      //     'content-type': 'application/json'
-      //   },
-      //   success: function (res) {
-      //     //从数据库获取用户信息
-      //     that.queryUsreInfo();
-      //     console.log("插入小程序登录用户信息成功！");
-      //   }
-      // });
-      //授权成功后，跳转进入小程序首页
-      wx.switchTab({
-        url: '/pages/index/index'
-      })
-    } else {
-      //用户按了拒绝按钮
-      wx.showModal({
-        title: '警告',
-        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
-        showCancel: false,
-        confirmText: '返回授权',
-        success: function (res) {
-          if (res.confirm) {
-            console.log('用户点击了“返回授权”')
-          }
-        }
-      })
-    }
-  },
-  //获取用户信息接口
-  queryUsreInfo: function () {
-    // wx.request({
-    //   url: app.globalData.urlPath + 'user/userInfo',
-    //   data: {
-    //     openid: app.globalData.openid
-    //   },
-    //   header: {
-    //     'content-type': 'application/json'
-    //   },
-    //   success: function (res) {
-    //     console.log(res.data);
-    //     getApp().globalData.userInfo = res.data;
-    //   }
-    // })
-    
-  },
 
+  showSettingToast: function (e) {
+    wx.showModal({
+      title: '提示！',
+      confirmText: '去设置',
+      showCancel: false,
+      content: e,
+      success: function (res) {
+        if (res.confirm) {
+          wx.navigateTo({
+            url: '../setting/setting',
+          })
+        }
+      }
+    })
+  },
 })
