7200fc92fe67414b24f891ea2c610872619f0f2d
changjie <regretn@163.com>
2019-09-30 00:31:20 +0800

    saa

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,15 +12,14 @@ Page({
     duration: 500,
     sectionicon: ['../../images/3.png', '../../images/2.png'
       , '../../images/1.png','../../images/guanggao.png'],
-    activearr: [{ id: 1, img: 'http://www.scope.com.cn/upload/page/2016/0803/14702086354975.jpg', title: "SCOPE  昌乐一中翻转课堂亮相中国-东盟交流周", section: "第九届“中国-东盟教育交流周”在贵阳国际生态会议中心开幕，刘延东副总理出席开幕式并发表演讲。" }, {
-      id: 2, img: 'http://www.scope.com.cn/upload/page/2016/1019/14768623118180.jpg', title: "国务院副总理汪洋、商务部部长及乌拉圭总统莅临希科普广交会展位 视察指导工作", section: "2016年10月15日—19日，120届广交会在广州市隆重举行。展会期间，中央政治局委员、国务院副总理汪洋，商务部国际贸易谈判代表兼副部长钟山及乌拉圭总统，分别于16日、17日莅临我司广交会展位视察指导工作。" }]
+    activearr: [{ id: 99, img: 'http://www.scope.com.cn/upload/page/2016/0803/14702086354975.jpg', title: "SCOPE  昌乐一中翻转课堂亮相中国-东盟交流周", section: "第九届“中国-东盟教育交流周”在贵阳国际生态会议中心开幕，刘延东副总理出席开幕式并发表演讲。", link: "http://www.scope.com.cn/content.php" }, {
+      id: 96, img: 'http://www.scope.com.cn/upload/page/2016/1019/14768623118180.jpg', title: "国务院副总理汪洋、商务部部长及乌拉圭总统莅临希科普广交会展位 视察指导工作", section: "2016年10月15日—19日，120届广交会在广州市隆重举行。展会期间，中央政治局委员、国务院副总理汪洋，商务部国际贸易谈判代表兼副部长钟山及乌拉圭总统，分别于16日、17日莅临我司广交会展位视察指导工作。", link: "http://www.scope.com.cn/content.php" }]
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function () {
-    console.log('hello world')
   },
 
   /**
@@ -79,6 +78,12 @@ Page({
   tologistics: function(){
     wx.navigateTo({
       url: '../Logistics/Logistics'
+    })
+  },
+  jinru: function(e) {
+    console.log(e.target.dataset)
+    wx.navigateTo({
+      url: `../WebView/WebView?link=${e.target.dataset.name}&id=${e.target.dataset.id}`,
     })
   }
 })
